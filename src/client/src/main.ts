d6713a9344b43238dddac15c08dac78d747eebb1
Daniel Flanagan <daniel@lytedev.io>
2018-03-14 16:56:35 -0500

    Refactoring store for proper use with typescript

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import CloudLampStore from './CloudLampStore';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+	store: CloudLampStore(),
+	router,
+	render: (h) => h(App),
 }).$mount('#app');
